07177073cd9f1be1450cb9aecaaef59395b3fdde
angx7 <angelmirocu@gmail.com>
2025-06-06 17:48:11 -0600

    initial commit

---
+++ ChambaApp-Front/ChambaApp_FrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChambaApp_FrontApp.swift
+//  ChambaApp-Front
+//
+//  Created by Angel Becerra Rojas on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChambaApp_FrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChambaApp-Front/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChambaApp-Front
+//
+//  Created by Angel Becerra Rojas on 06/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
